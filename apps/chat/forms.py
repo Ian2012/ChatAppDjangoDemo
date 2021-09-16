11f1ddabb7d7443593fd2eba0060a5a33b0ea3eb
Ian2012 <crisgarta8@gmail.com>
2021-09-16 09:53:48 -0500

    Bootstrap añadido al proyecto. Estilos añadidos a todas las vistas.

--- apps/chat/forms.py
+++ apps/chat/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -6,3 +7,10 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'password1', 'password2')
+        widget = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
