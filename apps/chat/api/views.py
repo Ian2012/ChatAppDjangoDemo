5977e71b62be97fc43ba0595a4e8849912a3a9e9
Ian2012 <crisgarta8@gmail.com>
2021-09-15 22:09:09 -0500

    Paginación de los mensajes terminada. Scroll terminado.

--- apps/chat/api/views.py
+++ apps/chat/api/views.py
@@ -1,3 +1,4 @@
+from rest_framework import pagination
 from rest_framework.generics import DestroyAPIView, ListCreateAPIView, get_object_or_404
 
 from apps.chat.api.serializers import MessageSerializer
@@ -12,6 +13,13 @@ class DeleteMessageAPIView(DestroyAPIView):
         return get_object_or_404(Message, id=self.request.query_params.get('detalle'))
 
 
+class PageNumberPagination(pagination.PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 5
+
+
 class ListCreateMessageAPIView(ListCreateAPIView):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+    pagination_class = PageNumberPagination
