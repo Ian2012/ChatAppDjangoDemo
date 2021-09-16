4fae4ddc6b6fa96b40bd859f7d2e94be7852b3a0
Ian2012 <crisgarta8@gmail.com>
2021-09-15 20:48:07 -0500

    Al eliminar y al enviar un mensaje (imagen) se notifica a todos los usuarios y ejecutan la acción correspondiente

--- apps/chat/consumers.py
+++ apps/chat/consumers.py
@@ -27,23 +27,34 @@ class ChatConsumer(WebsocketConsumer):
     # Receive message from WebSocket
     def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
-        pk = text_data_json['id']
-        user = text_data_json['user']
-        username = text_data_json['username']
-        image = text_data_json['image']
-        timestamp = text_data_json['timestamp']
-        # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'id': pk,
-                'user': user,
-                'username': username,
-                'image': image,
-                'timestamp': timestamp
-            }
-        )
+        event_type = text_data_json['type']
+        if event_type == 'chat_message':
+            pk = text_data_json['id']
+            user = text_data_json['user']
+            username = text_data_json['username']
+            image = text_data_json['image']
+            timestamp = text_data_json['timestamp']
+            # Send message to room group
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'id': pk,
+                    'user': user,
+                    'username': username,
+                    'image': image,
+                    'timestamp': timestamp
+                }
+            )
+        else:
+            pk = text_data_json['id']
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    'type': 'delete_message',
+                    'id': pk,
+                }
+            )
 
     # Receive message from room group
     def chat_message(self, event):
@@ -54,9 +65,18 @@ class ChatConsumer(WebsocketConsumer):
         timestamp = event['timestamp']
         # Send message to WebSocket
         self.send(text_data=json.dumps({
+            'type': 'chat_message',
             'id': pk,
             'user': user,
             'username': username,
             'image': image,
             'timestamp': timestamp
+        }))
+
+    def delete_message(self, event):
+        pk = event['id']
+        # Send message to WebSocket
+        self.send(text_data=json.dumps({
+            'type': 'delete_message',
+            'id': pk,
         }))
